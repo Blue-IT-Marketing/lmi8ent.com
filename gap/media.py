f8b639c204c10234cff21611e55efa258d8830c1
justice <mobiusndou@gmail.com>
2019-11-25 13:48:54 +0200

    updated
    backend for music and video files

---
+++ gap/media.py
@@ -0,0 +1,129 @@
+import os
+import webapp2
+import jinja2
+from google.appengine.ext import ndb
+from google.appengine.api import users
+import logging,json
+
+class Media(ndb.Expando):
+    uid = ndb.StringProperty()
+    filename = ndb.StringProperty()
+    description = ndb.StringProperty()
+    art = ndb.StringProperty()
+    size = ndb.StringProperty()
+    location = ndb.StringProperty()
+
+    def create_id(self, size=64, chars=string.ascii_lowercase + string.digits):
+        return ''.join(random.choice(chars) for x in range(size))
+
+
+class Music (Media):    
+    mid = ndb.StringProperty()
+
+    
+    def addMusic(self,music):
+        
+        music_instance = Music()
+        music_instance.uid = music['uid']
+        music_instance.mid = self.create_id()
+        music_instance.filename = music['filename']
+        music_instance.description = music['description']
+        music_instance.art = music['art']
+        music_instance.size = music['size']
+        music_instance.location = music['location']
+
+        music_instance.put()
+
+
+
+    def removeMusic(self,mid):
+        
+        music_query = Music.query(Music.mid == mid)
+        music_list = music_query.fetch()
+
+        for music in music_list:
+            music.key.delete()
+        
+        return music
+
+
+    def updateMusic(self,music):
+
+        music_query = Music.query(Music.mid == music['mid'])
+        music_list = music_query.fetch()
+
+        if (len(music_list) > 0):
+            music_file = music_list[0]
+            if (music_file.uid == music['uid']):
+                music_file.filename = music['filename']
+                music_file.description = music['description']
+                music_file.art = music['art']
+                music_file.size = music['size']
+                music_file.location = music['location']
+                music_file.put()
+
+                return music_file
+            else:
+                return ''
+        else:
+            return ''
+    # get a specifc music file
+    def getMusicFile(self,mid):
+        
+        music_query = Music.query(Music.mid == mid)
+        music_list = music_query.fetch()
+
+        if len(music_list) > 0:
+            return music_list[0]
+        else:
+            return ''
+
+    # get all music files
+    def getAllMusicFiles(self,uid):
+
+        music_query = Music.query(Music.uid == uid)
+        music_list = music_query.fetch()
+
+        return music_list
+
+
+        
+
+class Videos (Media):
+    vid = ndb.StringProperty()
+
+    def addVideo(self,video):
+        
+        video_instance = Videos()
+        video_instance.uid = Video['uid']
+        video_instance.vid = self.create_id()
+        video_instance.filename = Video['filename']
+        video_instance.description = Video['description']
+        video_instance.art = Video['art']
+        video_instance.size = Video['size']
+        video_instance.location = Video['location']
+        video_instance.put()
+
+
+    def removeVideo(self,vid):
+        
+        video_query = Videos.query(Videos.vid == vid)
+        video_list = video_query.fetch()
+
+        for video in video_list:
+            video.key.delete()
+
+        return video
+        
+
+    def updateVideo(self,video):
+        pass
+
+    def getVideoFiles(self,vid):
+        pass
+
+    def getAllVideoFiles(self,vid):
+        pass
+
+
+    
